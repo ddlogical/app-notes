3aa7948362abb7e8d05362a36db42ee40eb2e813
didenko.dma <didenko.dma@gmail.com>
2023-05-07 14:34:59 +0300

    Create breakpoints and make app adaptive

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,7 @@
 import styles from './Sidebar.module.scss';
 import ListItem from '../ListItem/ListItem';
+import Icon from '../Icon/Icon';
+import Button from '../Button/Button';
 import { DataContext } from '../../context/dataContext';
 import { StatusContext } from '../../context/statusContext';
 import { SearchContext } from '../../context/searchContext';
@@ -8,7 +10,8 @@ import { useNavigate } from 'react-router-dom';
 
 function Sidebar() {
   const {data} = useContext(DataContext);
-  const [dataArr, setDataArr] = useState([])
+  const [dataArr, setDataArr] = useState([]);
+  const [menuShown, setMenuShown] = useState(false);
   const {edit, changeStatus} = useContext(StatusContext);
   const {searchPhrase, searchedData} = useContext(SearchContext);
   const navigate = useNavigate();
@@ -21,20 +24,27 @@ function Sidebar() {
     }
   }, [data, searchedData, searchPhrase]);
   
-  const clickHandler = () => {
+  const clickSidebarHandler = () => {
     if(edit) {
       changeStatus('edit');
     }
     navigate(`/notes`);
   }
 
-  return <aside className={styles.sidebar}>
-    <div className={styles.sidebarContent} onClick={clickHandler}>
+  const clickMenuHandler = () => {
+    setMenuShown(prev => !prev);
+  }
+
+  return <>
+  <aside className={`${styles.sidebar} ${menuShown ? styles.menuShown: ''}`}>
+    <div className={styles.sidebarContent} onClick={clickSidebarHandler}>
       <ul className={styles.sidebarList}>
         {dataArr.map((elem, id) => <ListItem key={id} dataObj={elem} id={id + 1}/>).reverse()}
       </ul>    
-    </div> 
-  </aside>;
+    </div>
+  </aside>
+  <Button text={<Icon type={'menu'} />} className={styles.sidebarShown} handleClick={clickMenuHandler}/>
+  </>
 }
 
 export default Sidebar;
